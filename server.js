c8d93c1572cffd9c05c81abbad0b943ce454aaa4
Ciprian Racu <cypsciprian@gmail.com>
2021-05-26 17:30:43 +0300

    setup front end

--- server.js
+++ server.js
@@ -1,8 +1,44 @@
 const express = require("express");
+const { config } = require("dotenv");
 
+/**
+ * Creates the express app
+ */
 const app = express();
+
+/**
+ * Defines the port the app will run on
+ */
 const PORT = process.env.PORT || 5000;
 
+/**
+ * Handles connecting to the db
+ */
+const connectDB = require("./config/db");
+
+/**
+ * Dot ENV Config
+ */
+config();
+
+/**
+ * Connect database
+ */
+connectDB();
+
+/**
+ * Init Middleware
+ */
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("api running"));
+
+/**
+ * Define routes
+ */
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
