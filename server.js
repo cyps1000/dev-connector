f15ec97ee80b9fd9edfdaede88dada22c92fd7e5
Ciprian Racu <cypsciprian@gmail.com>
2021-05-28 10:38:27 +0300

    prepare for deployment

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { config } = require("dotenv");
 
+const path = require("path");
+
 /**
  * Creates the express app
  */
@@ -31,8 +33,6 @@ connectDB();
  */
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("api running"));
-
 /**
  * Define routes
  */
@@ -40,5 +40,19 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+/**
+ * Serve static assets in production
+ */
+if (process.env.NODE_ENV === "production") {
+  /**
+   * Set static folder
+   */
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
